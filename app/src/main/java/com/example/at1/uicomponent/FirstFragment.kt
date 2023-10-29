8d6700c52e7aa5f23a0b9f4a7f16522bfe777a36
abhishek7738127700 <abhishektiwaribecse@gmail.com>
2023-10-29 12:41:15 +0530

    This is my third commit

--- app/src/main/java/com/example/at1/uicomponent/FirstFragment.kt
+++ app/src/main/java/com/example/at1/uicomponent/FirstFragment.kt
@@ -33,10 +33,10 @@ class FirstFragment : Fragment() {
 
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding?.root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
